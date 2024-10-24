8d9c307f45076fc4bf83d137440b3b9882bee814
ali-hamza-52 <alihamzabrw52@gmail.com>
2024-10-24 21:07:57 +0500

    remove herobanner

--- components/common/HeroBanner.tsx
+++ components/common/HeroBanner.tsx
@@ -6,14 +6,14 @@ import { getDate, getTime } from "@/utils/getDateAndTime";
 const HeroBanner = () => {
   return (
     <div className="w-full relative py-3">
-      <Image
+      {/* <Image
         src={"/images/hero-bg.svg"}
         height={1200}
         width={1280}
         className="w-[1280px] rounded-2xl"
         alt="Hero"
         quality={100}
-      />
+      /> */}
 
       <div className="absolute flex flex-col gap-1 md:gap-3 bottom-6 md:bottom-20 left-6 md:left-10 text-white">
         <Typography variant="h2" weight="bold" className="text-white">
